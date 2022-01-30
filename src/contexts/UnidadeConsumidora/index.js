c39f62545d02d593fc078aba72afefc1fd60bd8f
Renato Bastos <renato.bastos96@gmail.com>
2022-01-30 15:39:59 -0300

    Update index.js

--- src/contexts/UnidadeConsumidora/index.js
+++ src/contexts/UnidadeConsumidora/index.js
@@ -1,51 +1,40 @@
-import {createContext, useEffect, useState} from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const UnidadeConsumidoraContext = createContext([]);
 
-export function UnidadeConsumidoraProvider({children}){
+export function UnidadeConsumidoraProvider({ children }) {
+  const [UnidadesConsumidoras, setUnidadesConsumidoras] = useState([]);
+  const [atualizar, setAtualizar] = useState(0);
 
-    const [UnidadesConsumidoras, setUnidadesConsumidoras] = useState([]);
-    const [atualizar, setAtualizar] = useState(0);
+  const [editValue, setEditValue] = useState();
 
-    const [editValue, setEditValue] = useState();
+  useEffect(async () => {
+    const response = await fetch("http://localhost:3333/unidades");
+    const unidadesResponse = await response.json();
+    setUnidadesConsumidoras(unidadesResponse);
+  }, [atualizar]);
 
-    useEffect( async () => {
-        const response = await fetch('http://localhost:3333/unidades');
-        const unidadesResponse = await response.json();
-        setUnidadesConsumidoras(unidadesResponse);
-        
+  const atualizarFetch = async () => {
+    const atualizacao = await fetch("http://localhost:3333/unidades");
+    setAtualizar(atualizacao);
+  };
 
-    },[atualizar]);
+  const editUnidade = async (id) => {
+    const responseedit = await fetch("http://localhost:3333/unidades/" + id);
+    const editJson = await responseedit.json();
+    setEditValue(editJson);
+  };
 
-    const atualizarFetch =  async () =>{
-        const atualizacao = await fetch('http://localhost:3333/unidades');
-        setAtualizar(atualizacao);
-
-    }
-    
-    const editUnidade = async (id) => {
-
-        const responseedit = await fetch('http://localhost:3333/unidades/'+id);
-        const editJson = await responseedit.json();
-        setEditValue(editJson);
-
-        
-
-    }
-
-    return(
-        <UnidadeConsumidoraContext.Provider
-        value={{
-            unidades: UnidadesConsumidoras,
-            atualizar : atualizarFetch,
-            editValue: editValue,
-            editUnidade: editUnidade
-
-        }}>
-        {children}
-        </UnidadeConsumidoraContext.Provider>
-
-    )
-
-
+  return (
+    <UnidadeConsumidoraContext.Provider
+      value={{
+        unidades: UnidadesConsumidoras,
+        atualizar: atualizarFetch,
+        editValue: editValue,
+        editUnidade: editUnidade,
+      }}
+    >
+      {children}
+    </UnidadeConsumidoraContext.Provider>
+  );
 }
